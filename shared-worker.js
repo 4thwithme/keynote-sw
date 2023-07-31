59cbe78de6412c227f2dc9cea9acf3a0da81a79e
4thwithme <aopopenko@gmail.com>
2023-07-31 12:32:21 +0200

    SW-1 copy

---
+++ shared-worker.js
@@ -0,0 +1,14 @@
+const BC = new BroadcastChannel("test");
+
+onconnect = (e) => {
+  const port = e.ports[0];
+
+  port.addEventListener("message", (e) => {
+    console.log("[WORKER THREAD] (Received message): ", e.data, e.ports);
+
+    const res = e.data.base ** e.data.pow;
+    BC.postMessage({ res, base: e.data.base, pow: e.data.pow });
+  });
+
+  port.start();
+};
